28629b296f93586904d2d182c4ed2bb19189b89c
Junnama Noda <junnama@alfasado.jp>
2015-10-14 00:21:24 +0900

    Change plugin name and in_array in PHP.

--- plugins/GetHashVar/php/block.mtifinarray.php
+++ plugins/GetHashVar/php/block.mtifinarray.php
@@ -2,11 +2,13 @@
 function smarty_block_mtifinarray ( $args, $content, &$ctx, &$repeat ) {
     if (! isset( $content ) ) {
         if ( isset( $args[ 'name' ] ) ) $name = $args[ 'name' ];
-        if ( isset( $args[ 'value' ] ) ) $key = $args[ 'value' ];
+        if ( isset( $args[ 'value' ] ) ) $value = $args[ 'value' ];
         if ( isset( $args[ 'var' ] ) ) $value = $args[ 'var' ];
         $array = $ctx->__stash[ 'vars' ][ $name ];
         if ( is_array( $array ) ) {
-            return $ctx->_hdlr_if( $args, $content, $ctx, $repeat, TRUE );
+            if ( in_array( $value, $array ) ) {
+                return $ctx->_hdlr_if( $args, $content, $ctx, $repeat, TRUE );
+            }
         }
         return $ctx->_hdlr_if( $args, $content, $ctx, $repeat, FALSE );
     } else {
