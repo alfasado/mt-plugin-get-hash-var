883959934335d5d9e587bce92e4c45c74be5d169
Junnama Noda <junnama@alfasado.jp>
2015-10-17 17:12:40 +0900

    Add. Function DeleteVars and modifier mtignore.

---
+++ plugins/GetHashVar/php/function.mtdeletevars.php
@@ -0,0 +1,27 @@
+<?php
+function smarty_function_mtdeletevars ( $args, &$ctx ) {
+    if ( isset( $args[ 'name' ] ) ) $name = $args[ 'name' ];
+    if (! name ) return '';
+    $names = array();
+    if (! is_array( $name ) ) {
+        if ( strpos( $name, ':' ) !== FALSE ) {
+            $_names = str_getcsv( $name, ':' );
+            $__names = array();
+            foreach ( $_names as $_name ) {
+                if ( strpos( $_name, 'Array.' ) === 0 ) {
+                    $_name = str_replace( 'Array.', '', $_name );
+                    $_name = $ctx->__stash[ 'vars' ][ $_name ];
+                }
+                array_push( $__names, $_name );
+            }
+            $names = $__names;
+        } else if ( strpos( $name, ' ' ) !== FALSE ) {
+            $names = explode( ' ', $name );
+        }
+    }
+    foreach( $names as $name ) {
+        $name = trim( $name );
+        unset( $ctx->__stash[ 'vars' ][ $name ] );
+    }
+}
+?>
---
+++ plugins/GetHashVar/php/modifier.mtignore.php
@@ -0,0 +1,5 @@
+<?php
+function smarty_modifier_mtignore( $taxt, $arg ) {
+    return '';
+}
+?>
