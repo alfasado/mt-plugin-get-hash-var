7d8b3eabd66ddd3342697c7612c6f714aefbd5a8
Junnama Noda <junnama@alfasado.jp>
2015-10-24 17:00:23 +0900

    MTArraySort sort_order="ascend".

--- plugins/GetHashVar/php/function.mtarraysort.php
+++ plugins/GetHashVar/php/function.mtarraysort.php
@@ -4,6 +4,7 @@ function smarty_function_mtarraysort ( $args, &$ctx ) {
     if ( isset( $args[ 'kind' ] ) ) $kind = $args[ 'kind' ];
     if ( isset( $args[ 'scope' ] ) ) $scope = $args[ 'scope' ];
     if ( isset( $args[ 'order' ] ) ) $order = $args[ 'order' ];
+    if ( isset( $args[ 'sort_order' ] ) ) $order = $args[ 'sort_order' ];
     if ( $scope == 'value' ) {
         $scope = 'var';
     } else if (! $scope ) {
