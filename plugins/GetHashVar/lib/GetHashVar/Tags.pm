a3e1c430487dc3c96ad560ca564b9f1b61245849
Hideki Abe <hideki.abe.0630@gmail.com>
2016-02-26 17:41:46 +0900

    Fix _hdlr_array_join arg name

--- plugins/GetHashVar/lib/GetHashVar/Tags.pm
+++ plugins/GetHashVar/lib/GetHashVar/Tags.pm
@@ -453,7 +453,7 @@ sub _hdlr_get_hash_key {
 sub _hdlr_array_join {
     my ( $ctx, $args, $cond ) = @_;
     my $name = $args->{ 'name' } || return '';
-    my $glue  = $args->{ 'glue ' } || '';
+    my $glue  = $args->{ 'glue' } || '';
     my $array = $ctx->stash( 'vars' )->{ $name };
     $array = $ctx->stash( 'vars' )->{ lc ( $name ) } unless $array;
     if ( ( ref $array ) eq 'ARRAY' ) {
